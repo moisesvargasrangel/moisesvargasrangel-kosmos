37630d422840b1044d2b6e44cd3078499b4062f8
Moisés Vargas Rangel <vargasrangelaudio@gmail>
2022-09-14 00:14:59 -0500

    Tareas OK

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+  import logo from './logo.svg';
+  import './App.css';
 
-//Components
-import Navbar from './components/navbar';
+  //Components
+  import Navbar from './components/navbar/navbar';
+  import Todo from './components/Todo/Todo';
+  function App() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <Todo/>
+      </div>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  export default App;
